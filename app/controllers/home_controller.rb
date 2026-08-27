ce91a916438cbdf5135fe0b3b2443111b63d0610
Abdullah Johar <your-email@example.com>
2026-08-27 23:23:33 +0500

    Update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,14 @@ class HomeController < ApplicationController
     @service_categories = ServiceCategory
       .where(active: true)
       .order(:name)
+
+    @featured_providers = ProviderProfile
+      .where(approval_status: :approved)
+      .includes(
+        :user,
+        provider_services: :service_category
+      )
+      .order(:business_name)
+      .limit(3)
   end
 end
